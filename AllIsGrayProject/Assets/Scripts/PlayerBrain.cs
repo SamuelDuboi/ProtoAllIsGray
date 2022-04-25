e7dfaa858041a71bb5d1818c41776cdf5b911f4c
sam <s.dubois@rubika-edu.com>
2022-04-25 12:49:16 +0200

    GA packages

--- AllIsGrayProject/Assets/Scripts/PlayerBrain.cs
+++ AllIsGrayProject/Assets/Scripts/PlayerBrain.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PlayerBrain : MonoBehaviour
 {
     public NavMeshAgent myAgent;
+    public PlayerInput player;
     
     // Update is called once per frame
     void Update()
