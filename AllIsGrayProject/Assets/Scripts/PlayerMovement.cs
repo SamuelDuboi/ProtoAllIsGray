fc748a0dd9a88ae248b17e6dc73b91159b491d10
sam <s.dubois@rubika-edu.com>
2022-04-26 11:39:03 +0200

    input and controller WIP

---
+++ AllIsGrayProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+public class PlayerMovement : Movable
+{
+    public Rigidbody rigidbody;
+    public Transform myTransform;
+    float rotation;
+    bool IsRotating;
+    [Range(0,0.5f)]
+    public float rotationTreshold;
+
+    [Range(0, 0.5f)]
+    public float movementTreshold;
+
+    bool isMoving;
+    float movementValue;
+    public float jetpackForce = 1;
+    public float jetpackMaxTime = 1;
+    public float jetpackCooldown = 2;
+    private float jetpackCurrentUse;
+    public float minjetPackValue = 0.1f;
+    private bool isCoolingDown;
+    private float currentCoolingValue;
+    public Image CoolDownImage;
+    public void Rotation(InputAction.CallbackContext context)
+    {
+        rotation = context.ReadValue<Vector2>().x;
+        IsRotating = Mathf.Abs(rotation) > rotationTreshold;
+    }
+    public void Impulse(InputAction.CallbackContext context)
+    {
+        movementValue = context.ReadValue<float>();
+        isMoving = Mathf.Abs(movementValue) > movementTreshold;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsRotating)
+            transform.Rotate(0, 0, rotation, Space.Self);
+        if (isMoving && jetpackCurrentUse <= jetpackMaxTime)
+        {
+            if (!CoolDownImage.gameObject.activeSelf)
+                CoolDownImage.gameObject.SetActive(true);
+            rigidbody.AddForce(myTransform.up * jetpackForce, ForceMode.Impulse);
+            jetpackCurrentUse += Time.deltaTime;
+            
+        }
+        else if (jetpackCurrentUse <1- minjetPackValue && jetpackCurrentUse>0)
+            jetpackCurrentUse -= Time.deltaTime;
+        else if(isMoving)
+        {
+            isCoolingDown = true;
+            CoolDownImage.color = Color.red;
+        }
+
+        if (isCoolingDown)
+        {
+            currentCoolingValue += Time.deltaTime;
+            CoolDownImage.fillAmount = currentCoolingValue / jetpackCooldown;
+            if (currentCoolingValue>= jetpackCooldown)
+            {
+                isCoolingDown = false;
+                currentCoolingValue = 0;
+                CoolDownImage.color = Color.green;
+                jetpackCurrentUse = 0;
+            }    
+        }
+        else
+            CoolDownImage.fillAmount = 1 - jetpackCurrentUse / jetpackMaxTime;
+    }
+}
