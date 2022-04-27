9ac98396ca011b03f3c5c480f1fd64c37227583e
Aubane <a.rittano@rubika-edu.com>
2022-04-27 16:01:38 +0200

    integration vfx jetpack

--- AllIsGrayProject/Assets/Scripts/Player/PlayerMovement.cs
+++ AllIsGrayProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -25,6 +25,12 @@ public class PlayerMovement : Movable
     private float currentCoolingValue;
     public Image CoolDownImage;
     public bool doCoolDown;
+
+    public TrailRenderer jetpackTrail;
+    public ParticleSystem jetpackCircles;
+    public ParticleSystem jetpackFlames;
+    public GameObject jetpackBigFlame;
+
     public void Rotation(InputAction.CallbackContext context)
     {
         rotation = context.ReadValue<Vector2>().x;
@@ -34,12 +40,30 @@ public class PlayerMovement : Movable
     {
         movementValue = context.ReadValue<float>();
         isMoving = Mathf.Abs(movementValue) > movementTreshold;
+
+        if (context.started)
+        {
+            jetpackBigFlame.SetActive(true);
+            jetpackFlames.Play();
+            jetpackCircles.Play();
+            jetpackTrail.emitting = true;
+        }
+        else if (context.canceled)
+        {
+            jetpackBigFlame.SetActive(false);
+            jetpackFlames.Stop();
+            jetpackCircles.Stop();
+            jetpackTrail.emitting = false;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        jetpackBigFlame.SetActive(false);
+        jetpackFlames.Stop();
+        jetpackCircles.Stop();
+        jetpackTrail.emitting = false;
     }
 
     // Update is called once per frame
@@ -73,6 +97,8 @@ public class PlayerMovement : Movable
             {
                 currentCoolingValue += Time.deltaTime;
                 CoolDownImage.fillAmount = currentCoolingValue / jetpackCooldown;
+
+
                 if (currentCoolingValue >= jetpackCooldown)
                 {
                     isCoolingDown = false;
